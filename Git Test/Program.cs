689003d099b7043e3df4b00f0e98559413693960
binilbpx <binilbpx@gmail.com>
2020-02-11 18:07:19 +0530

    Changed

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -6,7 +6,7 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Text Changed..");
         }
     }
 }
